09fd81a0b51df8668a123f5c95c895d8450c0fd4
Miguel Figuera <miguelqui725@gmail.com>
2023-03-15 23:46:40 -0400

    library done, with erase and update buttons functioning properly

--- book_constructor.js
+++ book_constructor.js
@@ -1,8 +1,9 @@
-function Book(author, title, pages, read) {
+function Book(author, title, pages, read,index) {
   this.author = author;
   this.title = title;
   this.pages = pages;
   this.read = read;
+  this.index=index;
 }
 
 
@@ -16,14 +17,14 @@ function addBook(book) {
   library.push(book);
 }
 
-let frist=new Book('Tolkien',"The two towers","5",true)
-let second=new Book("Jack London","White Fang","232",true)
-let third=new Book("George Lucas","Star Wars","500",true)
-let fourth=new Book("Miguel de Cervantes","Don Quijote","900",false)
-
-library.push(frist, second, third,fourth)
+let frist=new Book('Tolkien',"The two towers","5",true,0)
+let second=new Book("Jack London","White Fang","232",true,1)
+let third=new Book("George Lucas","Star Wars","500",true,2)
+let fourth=new Book("Miguel de Cervantes","Don Quijote","900",false,3)
+let fifth=new Book("Lord O'malley","Straight From Heaven","455",false,4)
+library.push(frist, second, third,fourth,fifth)
 renderingBook()
-libraryIndex+=4
+libraryIndex+=5
 
 
 let form=document.getElementById("bookForm")
@@ -59,6 +60,9 @@ form.addEventListener("submit", (event) => {
 
     // Pushes Obj to library array.
     addBook(newBook)
+    //adding data atribute for the index
+    newBook.index=library.indexOf(newBook);
+
     renderingBook()
     libraryIndex+=1
     // Reset the form fields after submission.
@@ -66,6 +70,17 @@ form.addEventListener("submit", (event) => {
     toggleForm()
     changeName()
 })
+
+//functions for updated the status, erase from library, and reset index
+//reset index is to keep things organized
+Book.prototype.updateStatus= function(){this.read==true?this.read=false:this.read=true;}
+Book.prototype.eraseFromRendering=function(){library.splice(this.index,1)}
+
+let resetIndex=function(){
+    for(let i=0;i<library.length;i++){
+        library[i].index=i;
+    }
+}
 
 // Rendering the books
 function renderingBook(){
@@ -77,18 +92,34 @@ function renderingBook(){
         let author=document.createElement("h4")
         let pages=document.createElement("p")
         let readed=document.createElement("p")
+        let deleteButton=document.createElement("button")
+        let updatedReaded=document.createElement("button")
 
+        updatedReaded.innerText="Update status"
+        deleteButton.innerText= "Delete this book"
         title.innerText=e.title
         author.innerText=e.author
         pages.innerText=e.pages
-        e.read == true ? readed.innerText= "Yeah, readed!" : readed.innerText= "Not yet readed";
+        let updatedRead= function(e) {e== true ? readed.innerText= "Yeah, readed!" : readed.innerText= "Not yet readed";}
+        updatedRead(e.read)
 
+        card.id=e.index
         card.appendChild(title)
         card.appendChild(author)
         card.appendChild(pages)
         card.appendChild(readed)
+        card.appendChild(updatedReaded)
+        card.appendChild(deleteButton)
+        
+        bookContainer=document.getElementById("book-container")
+        bookContainer.appendChild(card)
 
-        document.getElementById("book-container").appendChild(card)
-        alert(`Book ${e.title} has been created!`)
+
+        updatedReaded.onclick=()=>{e.updateStatus(),updatedRead(e.read)};
+        deleteButton.onclick=()=>{library.splice(e.index,1),card.remove(),resetIndex()};
+        //I used remove() to pull the card out of the book-container div
+
     }
 }
+
+
